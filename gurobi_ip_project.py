da5830e6644ec6aa0280b668a9193eefcaf77de2
rudolf <rudolf.lam@mail.mcgill.ca>
2016-04-05 10:38:20 -0400

    Added more constraints to improve cuts

--- gurobi_ip_project.py
+++ gurobi_ip_project.py
@@ -30,6 +30,8 @@ def build_model(edges,n,M,m):
     # create the model
     model = Model("Social Network Cascade")
     
+    print('Initiating model')
+    
     # create the variables
     # x[i,j] is 1 if app installed at step j else 0
     # y[i,j] is 1 if i has at least m friends who have the app installed at step j-1 else 0
@@ -47,8 +49,15 @@ def build_model(edges,n,M,m):
     # d is the deviation from the absolute target
     d = model.addVar(lb=0, ub=n-M, name='d')
     
+    # choice is a disjunction variable
+    choice = model.addVar(vtype=GRB.BINARY, name='choice')
+    
+    print('Constructed variables for model')
+    
     # integrate the variables into the model
     model.update()
+    
+    print('Pushed variables into model')
     
     # construct the parameter c 
     # c[i,j] is 1 if i is friends with j else 0
@@ -71,7 +80,19 @@ def build_model(edges,n,M,m):
             except: 
                 pass
     
+    print('Loaded friends web')
+    
+    # calculate the number of friends for each person
+    # and calculate if each person has less than m friends
+    f = [0]*n
+    ltm = [0]*n
+    for i, neighbours in edges.items():
+        f[i] = len(neighbours)
+        ltm[i] = 1 if f[i] < m else 0
+    
     # add constraints
+    
+    print('Loading constraints')
     
     def summation(list):
         return reduce(lambda acc,a: acc+a, list)
@@ -80,12 +101,14 @@ def build_model(edges,n,M,m):
     # sum of x[i][n-1] for i from 0 to n-1 >= M
     
     model.addConstr( summation([x[i][n-1] for i in range(n)]) >= M, 'RequiredNumberOfPeople' )
+    print('Loaded constraint 1')
     
     # Constraint 2
     # as many people as possible at the end
     # d + sum of x[i][n-1] for i from 0 to n-1 = n
     
     model.addConstr( d+summation([x[i][n-1] for i in range(n)]) == n , 'InfectAsManyPeopleAsPossible')
+    print('Loaded constraint 2')
     
     # Constraint 3
     # if person i has the app installed at period t then he has it installed for the period t+1
@@ -94,7 +117,8 @@ def build_model(edges,n,M,m):
     for i in range(n) :
         for t in range(1,n):
             model.addConstr( x[i][t-1] <= x[i][t], 'AppWillHauntYouForever')
-            
+    print('Loaded constraint 3')
+    
     # Constraint 4
     # if person i has at least m friends who have the app installed at time t-1 or earlier then person i will install app at time t
     # x[i][t] <= x[i][t-1] + y[i][t] for all i = 0,...,n-1 and t = 1,...,n-1
@@ -102,6 +126,7 @@ def build_model(edges,n,M,m):
     for i in range(n):
         for t in range(1,n):
             model.addConstr( x[i][t] <= x[i][t-1] + y[i][t], 'PeerPressure')
+    print('Loaded constraint 4')
     
     # Constraint 5
     # link x[i][j] and y[i][j]
@@ -111,6 +136,7 @@ def build_model(edges,n,M,m):
     for i in range(n):
         for t in range(1,n):
             model.addConstr( summation([c[i][j] * x[j][t-1] for j in range(n)]) >= m * y[i][t], 'Link1' )
+    print('Loaded constraint 5A')
     
     # part B
     # sum of c[i][k] * x[k][j-1] for k from 0 to n-1 <= m-1 + (n-m+1)*y[i][j] for all i=0,...,n-1 and j=1,...,n-1
@@ -118,6 +144,23 @@ def build_model(edges,n,M,m):
     for i in range(n):
         for t in range(1,n):
             model.addConstr( summation([c[i][k] * x[k][t-1] for k in range(n)]) <= m-1+ (n-m+1) * y[i][t], 'Link2' )
+    print('Loaded constraint 5B')
+    
+    # Constraint 6
+    # we require a minimum of m people to seed the process
+    # sum of x[i][0] >= m
+    
+    model.addConstr( summation([x[i][0] for i in range(n)]) >= m, 'MinimumSeed' )
+    print('Loaded constraint 6')
+    
+    # Constraint 7
+    # people who have less than m friends cannot be infected, so they either are infected on the first step or never at all
+    # sum of x[i][t] for t from 0 to n-1 == (n or 0) for all i with less than m friends 
+    
+    for i in range(n):
+        if ltm[i] == 1:
+            model.addConstr( summation([x[i][t] for t in range(n)]) == n*choice , 'NeverInfected')
+    print('Loaded constraint 7')
     
     # create the objective function
     # min weight1 * sum x[i][0] for i from 0 to n-1 + weight2 * d
@@ -127,9 +170,16 @@ def build_model(edges,n,M,m):
     w1 = 1+n-M
     w2 = 1
     model.setObjective(w1 * summation([x[i][0] for i in range(n)]) + w2 * d , GRB.MINIMIZE)
+    print('Loaded Objective')
     
     # flush everything into the model
     model.update()
+    
+    # MIP Start
+    # greedy algorithm
+    
+    
+    print('Finished building model')
     
     return model
 
@@ -140,6 +190,7 @@ if __name__=='__main__':
     parser.add_argument('friends_file', help='file defining the social network')
     parser.add_argument('-o', '--output', help='name the output file (default: solution.txt)')
     parser.add_argument('-m', '--model', help='save the model (after running solve) to a file')
+    parser.add_argument('--memory', action='store_true', help='activates NodefileStart param at 0.5Gb to reduce memory usage')
     args = parser.parse_args()
     # extract user input
     m, M, friends_file = args.m, args.M, args.friends_file
@@ -149,9 +200,12 @@ if __name__=='__main__':
     
     # build the model
     model = build_model(edges,n,M,m)
+    model.params.NodefileStart = 0.5 if args.memory else model.params.NodefileStart
     
     # find solution
-    solution =  model.optimize()
+    def callback(model, where):
+        print(str(model) +' called '+ str(where))
+    solution =  model.optimize(callback)
     
     for v in model.getVars():
         if 'x' in v.varName and ',0]' in v.varName:
